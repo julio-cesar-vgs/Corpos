3a2c6b7326d6eb0db7ae4ea1fe46e929bc942cf2
LABFAE\20312-5 <julio.cesar.vgs@gmail.com>
2018-05-03 19:54:38 -0300

    Primeiro Commit

---
+++ Corpos/Assets/ControlaBola.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaBola : MonoBehaviour {
+
+	//Variáveis a serem expostas no Unity
+	public float velocidade;
+
+	//Variáveis Globais reservadas ao processamento
+	private Rigidbody corpo;
+
+	// Use this for initialization
+	void Start () {
+		corpo = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 movimento = new Vector3 (horizontal, 0.0f, vertical);
+		corpo.AddForce(movimento * velocidade);
+	}
+
+	void OnTriggerEnter(Collider outro) {
+		if(outro.gameObject.CompareTag("Caixa")) {
+			outro.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Corpos/Assets/ControlaVisao.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaVisao : MonoBehaviour {
+
+	public GameObject bola;
+	private Vector3 desloca;
+
+	// Use this for initialization
+	void Start () {
+		desloca = transform.position - bola.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = bola.transform.position + desloca;
+	}
+}
